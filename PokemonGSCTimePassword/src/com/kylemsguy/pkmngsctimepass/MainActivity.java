cdd8725b9ee98bc192918986b4565600161ef2b9
Kyle <kylezhou2002@hotmail.com>
2014-07-22 15:09:01 -0700

    Basic functionality works

--- PokemonGSCTimePassword/src/com/kylemsguy/pkmngsctimepass/MainActivity.java
+++ PokemonGSCTimePassword/src/com/kylemsguy/pkmngsctimepass/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
@@ -24,8 +25,52 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 	
-	public void submitAction(View view){
+	public void helpTname(View view){
 		
+	}
+
+	public void submitAction(View view) {
+		String name;
+		int cash;
+		int train_id;
+		
+		int rawPasswd;
+
+		EditText nameField = (EditText) findViewById(R.id.trainer_name);
+		EditText moneyField = (EditText) findViewById(R.id.money);
+		EditText idField = (EditText) findViewById(R.id.trainer_id);
+		EditText resultField = (EditText) findViewById(R.id.passwd_result);
+
+		name = nameField.getText().toString();
+		try {
+			cash = Integer.parseInt(moneyField.getText().toString());
+		} catch (NumberFormatException e) {
+			moneyField.setError("Invalid Input");
+			return;
+		}
+		
+		try {
+			train_id = Integer.parseInt(idField.getText().toString());
+		} catch (NumberFormatException e) {
+			idField.setError("Invalid Input");
+			return;
+		}
+
+		try {
+			rawPasswd = PkmnGSCTools.genTimePass(name, cash, train_id);
+		} catch (Exception e) {
+			nameField.setError("Invalid character in Trainer Name");
+			return;
+		}
+		
+		StringBuffer passwd = new StringBuffer();
+		for(int i = 0; i < 4 - Math.log10(rawPasswd); i++){
+			passwd.append("0");
+		}
+		
+		passwd.append(Integer.toString(rawPasswd));
+		
+		resultField.setText(passwd.toString());
 	}
 
 	@Override
